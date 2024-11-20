1a2ea929f69fc3a18ef4fa2c57749236a7414945
Emiliano Rotta <emilianorottaf@gmail.com>
2024-11-20 19:34:52 -0300

    first commits

---
+++ app.js
@@ -0,0 +1,18 @@
+// Crear un servidor con Node.js.
+// El servidor debe responder a solicitudes HTTP para realizar operaciones sobre los datos.
+
+// Crear un archivo principal llamado app.js.
+// Este archivo será el punto de entrada del servidor y manejará todas las rutas necesarias.
+
+//en un archivo de datos llamado peliculas.json, estará almacenada la información de películas.
+
+//Implementar las siguientes funcionalidades:
+//CRUD completo:
+
+//Crear nuevas películas con datos como título, género, año y director.
+//Leer los datos completos del archivo y también los de una película específica por su ID o por su título.
+//Actualizar la información de una película existente, identificándola por su ID.
+//Eliminar una película del archivo, también identificándola por su ID.
+
+//La funcionalidad será probada en Postman para verificar el cumplimiento de todos los puntos.
+
